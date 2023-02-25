a523a08e2053c81fe2c91d6ed98d8d901ada239d
tankms <53538468+tankms@users.noreply.github.com>
2023-02-25 15:36:45 +0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,20 @@
+
+a = input("Please Choose One +,-, /, x")
+if a == "+":
+    st = int(input("Put in your first number"))
+    sc = int(input("Put in your second number"))
+    print(st + sc)   
+elif a == "-":
+     st = int(input("Put in your first number"))
+     sc = int(input("Put in your second number"))
+     print(st - sc)
+elif a == "/":
+      st = int(input("Put in your first number"))
+      sc = int(input("Put in your second number"))
+      print(st / sc)
+elif a == "x":
+      st = int(input("Put in your first number"))
+      sc = int(input("Put in your second number"))
+      print(st * sc)
+   
+    
